1819329be4db7e8a569987581e0d5d78e3274b81
Nicolas Puccio <puccio.nicolas.98@gmail.com>
2022-11-08 23:50:12 +0700

    move data fetching to Global module

--- src/Global.js
+++ src/Global.js
@@ -1,8 +1,45 @@
-const Globals = {}
-Globals.$stats = JSON.parse(localStorage.getItem('stats')) ?? [];
-Globals.$words = JSON.parse(localStorage.getItem('words')) ?? [];
-Globals.$songs = JSON.parse(localStorage.getItem('songs')) ?? [];
+exports.Globals = undefined;
 
-console.log(Globals)
+exports.fetchData = () => {
+    //page needs to be refreshed twice to see new changes in db
+    if (this.Globals)//data already initialized
+    {
+        console.log('globals already set up', this.Globals)
+        return
+    }
 
-export default Globals;
+    //fix should wait for backend before reading localstorage?
+    this.Globals = {}
+    this.Globals.$stats = JSON.parse(localStorage.getItem('stats')) ?? [];
+    this.Globals.$words = JSON.parse(localStorage.getItem('words')) ?? [];
+    this.Globals.$songs = JSON.parse(localStorage.getItem('songs')) ?? [];
+    console.log('setting up globals', this.Globals)
+
+    //hook called twice, how to avoid double request?
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = (event) => {
+        if (event.currentTarget.readyState === 4 && event.currentTarget.status === 200) {
+            localStorage.setItem('songs', request.responseText)
+            this.Globals.$songs = JSON.parse(request.responseText);
+        }
+    };
+    request.open('GET', 'http://localhost:3001/api/song/', true);
+
+    request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    request.setRequestHeader('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ1ZjkwNGVmMDE2ZGVjOGE3MTYwMTkiLCJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NjYxMDE0NjN9.55YtqF7GBtShk-MF6pY8DYVCMNypmXma_WEX6hK7QFA');
+    request.send();
+
+
+    const request2 = new XMLHttpRequest();
+    request2.onreadystatechange = (event) => {
+        if (event.currentTarget.readyState === 4 && event.currentTarget.status === 200) {
+            localStorage.setItem('words', request2.responseText)
+            this.Globals.$words = JSON.parse(request2.responseText);
+        }
+    };
+    request2.open('GET', 'http://localhost:3001/api/song/word', true);
+
+    request2.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    request2.setRequestHeader('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ1ZjkwNGVmMDE2ZGVjOGE3MTYwMTkiLCJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NjYxMDE0NjN9.55YtqF7GBtShk-MF6pY8DYVCMNypmXma_WEX6hK7QFA');
+    request2.send();
+}
--- src/components/songList.js
+++ src/components/songList.js
@@ -1,49 +1,14 @@
 import React from 'react';
 import SongDetail from './songDetail';
 import { Link } from 'react-router-dom';
-import Globals from '../Global';
+import {Globals} from '../Global';
 
 class SongList extends React.Component {
 
-    componentDidMount() {
-        //hook called twice, how to avoid double request?
-        const request = new XMLHttpRequest();
-        request.onreadystatechange = (event) => {
-            if (event.currentTarget.readyState === 4 && event.currentTarget.status === 200) {
-                Globals.$songs = JSON.parse(request.responseText);
-                localStorage.setItem('songs', JSON.stringify(Globals.$songs))
-                this.setState({ songs: Globals.$songs });
-            }
-        };
-        request.open('GET', 'http://localhost:3001/api/song/', true);
-
-        request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        request.setRequestHeader('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ1ZjkwNGVmMDE2ZGVjOGE3MTYwMTkiLCJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NjYxMDE0NjN9.55YtqF7GBtShk-MF6pY8DYVCMNypmXma_WEX6hK7QFA');
-        request.send();
-
-
-        const request2 = new XMLHttpRequest();
-        request2.onreadystatechange = (event) => {
-            if (event.currentTarget.readyState === 4 && event.currentTarget.status === 200) {
-                Globals.$words = JSON.parse(request2.responseText);
-                localStorage.setItem('words', JSON.stringify(Globals.$words))
-                this.setState({ words: Globals.$words });
-            }
-        };
-        request2.open('GET', 'http://localhost:3001/api/song/word', true);
-
-        request2.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        request2.setRequestHeader('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ1ZjkwNGVmMDE2ZGVjOGE3MTYwMTkiLCJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE2NjYxMDE0NjN9.55YtqF7GBtShk-MF6pY8DYVCMNypmXma_WEX6hK7QFA');
-        request2.send();
-    }
-
     render() {
-        if (!this.state || !this.state.words || !this.state.songs)//component not ready
-            return <h1>list loading</h1>
-
-        if (!this.state.song)//display list if no song was selected
+        if (!this.state?.song)//display list if no song was selected
         {
-            return this.state.songs.map((song) => {
+            return Globals.$songs.map((song) => {
                 song.known = song.review = song.new_ = song.total = 0;
                 const wordsFound = []
                 const date = new Date()
@@ -60,7 +25,7 @@ class SongList extends React.Component {
                         if (!stat)
                             song.new_++;
                         else {
-                            if (stat.score > 2)
+                            if (stat.score > 3)
                                 song.known++;
                             if (new Date(stat.next) < date)
                                 song.review++;
@@ -68,10 +33,10 @@ class SongList extends React.Component {
                     })
                 })
 
-                return <div key={song.title} className='songContainer'>
+                return <div key={song.title} className='song-container'>
                     <div style={{ width: '100%' }}>
-                        <Link to='/review' className='songContainer-link'>Review</Link>
-                        <h1 className='songContainer-h1' onClick={() => { this.setState({ song: song }) }}>{song.title}</h1>
+                        <Link to='/review' className='song-container-review-button'>Review</Link>
+                        <h1 className='song-container-h1' onClick={() => { this.setState({ song: song }) }}>{song.title}</h1>
 
                     </div>
                     <p style={{ marginTop: '10px', marginBottom: '5px' }}>{song.total} words</p>
@@ -88,7 +53,7 @@ class SongList extends React.Component {
 
         //display song if selected
         console.log(this.state.song)
-        return <SongDetail song={this.state.song} words={this.state.words} />
+        return <SongDetail song={this.state.song} />
     }
 }
 
