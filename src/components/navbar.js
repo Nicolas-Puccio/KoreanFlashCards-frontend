b95f592383c8eeaeb808b7f1c9fc05d84864450d
Nicolas-Puccio <puccio.nicolas.98@gmail.com>
2022-12-22 12:15:05 +1100

    small change for undefined/true state on navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,19 +3,18 @@ import { Link } from 'react-router-dom';
 import { Globals } from '../Global';
 
 class NavBar extends React.Component {
-    //state.showMenu is either undefined or true
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { showMenu: false };
     }
-    
+
     render() {
         return <>
             <div className='navbar'>
                 <Link className='navbar-logo' to='/' onClick={() => { Globals.SongList?.setState({ song: undefined }) }}>Home</Link>
                 <Link to='/review' onClick={() => { Globals.Review?.setState({ word: undefined }) }}>Review</Link>
-                <button onClick={() => this.setState({ showMenu: this.state.showMenu ? undefined : true })}>MENU</button>
+                <button onClick={() => this.setState({ showMenu: !this.state.showMenu })}>MENU</button>
             </div>
             {
                 this.state.showMenu &&
