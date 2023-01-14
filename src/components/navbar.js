25b1a8414e98d777c87f936060f1f4a0ed60215d
Nicolas-Puccio <puccio.nicolas.98@gmail.com>
2023-01-15 09:51:45 +1100

    turn navbar into function component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,32 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Globals } from '../Global';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Globals } from '../Global'
 
-class NavBar extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+export default function NavBar() {
 
-    render() {
-        return <>
-            <div className='navbar'>
-                <Link className='navbar-logo' to='/' onClick={() => { if (Globals.setSelectedSong) Globals.setSelectedSong(undefined) }}>Home</Link>
-                <Link to='/review' onClick={() => { if (Globals.setWordsToReview) Globals.setWordsToReview([]) }}>Review</Link>
-                <button onClick={() => this.setState({ showMenu: !this.state.showMenu })}>MENU</button>
+    const [showMenu, setShowMenu] = useState(false)
+
+    return (<>
+        <div className='navbar'>
+            <Link className='navbar-logo' to='/' onClick={() => { if (Globals.setSelectedSong) Globals.setSelectedSong(undefined) }}>Home</Link>
+            <Link to='/review' onClick={() => { if (Globals.setWordsToReview) Globals.setWordsToReview([]) }}>Review</Link>
+            <button onClick={() => setShowMenu(!showMenu)}>MENU</button>
+        </div>
+        {
+            showMenu &&//consider: should i close menu after clicking a Link?
+            <div className='navbar-menu'>
+                <Link to='/stats'>Stats</Link>
+                <Link to='/admin'>Admin</Link>
+                <Link to='/'>Logout</Link>
             </div>
-            {
-                //consider: should i close menu after clicking a Link?
-                this.state.showMenu &&
-                <div className='navbar-menu'>
-                    <Link to='/stats'>Stats</Link>
-                    <Link to='/admin'>Admin</Link>
-                    <Link to='/'>Logout</Link>
-                </div>
-            }
-        </>
-    }
+        }
+    </>)
 }
-
-export default NavBar;
