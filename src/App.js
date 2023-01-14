f73d40df802c0ef210cfb46efd26b86de3a90008
Nicolas-Puccio <puccio.nicolas.98@gmail.com>
2023-01-15 09:38:17 +1100

    turn App.js into function component

--- src/App.js
+++ src/App.js
@@ -5,73 +5,65 @@ import SongsPage from './pages/songs-page';
 import NavBar from './components/navbar';
 import Stats from './components/stats';
 import Admin from './components/admin';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Globals from './Global';
 
 
+export default function App() {
 
-class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  const [dataInitialized, setDataInitialized] = useState(false)
 
-  DataLoaded = () => {
-    this.setState({ data: true });
-  }
 
-  render() {
-    //consider: fix App rendering 4 times because of setState, should only be 3? or 2?
-    Globals.fetchData(this);
-    
-    if (this.state.data)
-      return <Routes>
-        <Route path='/' element={
-          <>
-            <NavBar />
-            <div className='main-div'>
-              <SongsPage />
-            </div>
-          </>
-        } />
-        <Route path='/review' element={
-          <>
-            <NavBar />
-            <div className='main-div'>
-              <ReviewPage />
-            </div>
-          </>
-        } />
-        <Route path='/test' element={
-          <>
-            <NavBar />
-            <div className='main-div'>
-              <TestPage />
-            </div>
-          </>
-        } />
-        <Route path='/stats' element={
-          <>
-            <NavBar />
-            <div className='main-div'>
-              <Stats />
-            </div>
-          </>
-        } />
-        <Route path='/admin' element={
-          <>
-            <NavBar />
-            <div className='main-div'>
-              <Admin />
-            </div>
-          </>
-        } />
-      </Routes>
-    else //fix: add proper loading screen
-      return <><h1>loading</h1></>
-  }
+  useEffect(() => {
+    Globals.fetchData(setDataInitialized)
+  }, [])
+
+
+  if (dataInitialized)//consider: should i set main-div on each component instead?
+    return <Routes>
+      <Route path='/' element={
+        <>
+          <NavBar />
+          <div className='main-div'>
+            <SongsPage />
+          </div>
+        </>
+      } />
+      <Route path='/review' element={
+        <>
+          <NavBar />
+          <div className='main-div'>
+            <ReviewPage />
+          </div>
+        </>
+      } />
+      <Route path='/test' element={
+        <>
+          <NavBar />
+          <div className='main-div'>
+            <TestPage />
+          </div>
+        </>
+      } />
+      <Route path='/stats' element={
+        <>
+          <NavBar />
+          <div className='main-div'>
+            <Stats />
+          </div>
+        </>
+      } />
+      <Route path='/admin' element={
+        <>
+          <NavBar />
+          <div className='main-div'>
+            <Admin />
+          </div>
+        </>
+      } />
+    </Routes>
+  else //fix: add proper loading screen
+    return <h1>loading</h1>
 }
-
-export default App;
