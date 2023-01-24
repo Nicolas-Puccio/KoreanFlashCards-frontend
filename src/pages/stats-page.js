eb31f028025a60ab59ac1989b10de178e81c4e36
Nicolas-Puccio <puccio.nicolas.98@gmail.com>
2023-01-24 19:46:37 +1100

    add basic stats page

--- src/pages/stats-page.js
+++ src/pages/stats-page.js
@@ -1,6 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Globals } from '../Global';
 
 export default function StatsPage() {
 
+    const [leaderboards, setLeaderboards] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/user/stats', { credentials: 'include' })
+            .then(async res => {
+                await res.json().then(json => {
+                    if (res.status !== 200)
+                        alert(json.message)
+                    else {
+                        json.forEach(json => json.data.sort((a, b) => b.reviewed - a.reviewed))
+                        setLeaderboards(json)
+                        console.log(json)
+                    }
+                })
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    return <>
+        <h2>My reviews</h2>
+        {
+            Globals.$stats.reviewed.map((reviewed, index) =>
+                <p key={index}>{reviewed.date} - {reviewed.reviewed.length}</p>
+            )
+        }
+
+        <br />
+        <br />
+
+        <h2>Leaderboard</h2>
+        {
+            leaderboards.map((leaderboard, index) => {
+                return (<div key={index}>
+                    <h3>{leaderboard.date.split('T')[0]}</h3>
+                    {
+                        leaderboard.data.map((data, index) =>
+                            <p key={`p${index}`}>{data.user} - {data.reviewed}</p>
+                        )
+                    }
+                    <br />
+                </div>)
+            })
+        }
+    </>
 }
