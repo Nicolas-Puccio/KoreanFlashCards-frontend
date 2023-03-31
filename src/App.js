0eecfb988780fb97ee54e45e22535eab93c78596
Puccio Nicolas <puccio.nicolas.98@gmail.com>
2023-03-31 15:53:55 +0700

    replace Router with MemoryRouter loads offline backup automatically if server is down

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import './App.css'
 import './style/navbar.css'
 import './style/songs-page.css'
 import './style/stats-page.css'
+
 import NavBar from './components/navbar'
+
 import ReviewPage from './pages/review-page'
 import SongsPage from './pages/songs-page'
 import StatsPage from './pages/stats-page'
 import AuthorizationPage from './pages/authorization-page'
+
+import { fetchData } from './Global'
+
+import { MemoryRouter } from 'react-router'
 import React, { useState, useEffect } from 'react'
+
+// removing Router import causes error even if not used
+// eslint-disable-next-line
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Globals, fetchData } from './Global'
 
 
 export default function App() {
 
   const [dataInitialized, setDataInitialized] = useState(false)
-  const [user, setUser] = useState(undefined) // const userExample = { admin: false, username: 'test' }
+  const [user, setUser] = useState(undefined) // const userExample = { admin: false, username: 'test' } // admin property not used for now
 
 
 
@@ -24,6 +32,8 @@ export default function App() {
    * Checks if the user has an active cookie
    */
   useEffect(() => {
+    fetchData(setDataInitialized)
+
     const cookie = document.cookie.split('; ').filter(row => row.startsWith('token=')).map(c => c.split('=')[1])[0]
 
     if (cookie) {
@@ -32,8 +42,6 @@ export default function App() {
         username: cookie.split('-')[1]
       })
     }
-
-    fetchData(setDataInitialized, cookie ? cookie.split('-')[1] : undefined)
   }, [])
 
 
@@ -41,11 +49,7 @@ export default function App() {
   if (!dataInitialized)
     return <h1>loading</h1> //fix: add proper loading screen
 
-  else if (Globals.$songs.length === 0 || Globals.$words.length === 0)
-    return <h1>server is not responding and there is no localStorage</h1> //fix:
-
-
-  return <Router>
+  return <MemoryRouter>
     <NavBar data={{ user, setUser }} />
     <div className='main-div'>
       <Routes>
@@ -56,5 +60,5 @@ export default function App() {
         <Route path='/login' element={<AuthorizationPage data={{ setUser }} />} />
       </Routes>
     </div>
-  </Router>
+  </MemoryRouter>
 }
