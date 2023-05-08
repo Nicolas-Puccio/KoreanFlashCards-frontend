50edddb61b4bd7cdd93b763135b586c21cc22ed1
Puccio Nicolas <puccio.nicolas.98@gmail.com>
2023-05-08 12:53:41 +0700

    add login request to api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 const URL = 'http://localhost:3001/api/' //'https://kfc-backend-eb38.onrender.com/api/'
+//-change url
 
 //gets songs and words
 export const getData = async () => {
@@ -6,9 +7,40 @@ export const getData = async () => {
     const res = await fetch(URL + 'song')
 
     const data = await res.json()
+    if (!res.ok)
+      throw new Error(data.message)
+
     return data
   } catch (error) {
     console.error(error)
-    throw new Error(error)
+
+    if (error.message === 'Failed to fetch')
+      return alert(error.message + ': please wait 2 minutes for the backend to spin up and try again')
+
+    alert(error.message)
+  }
+}
+
+export const login = async (action, body) => {
+  try {
+    const res = await fetch(`http://localhost:3001/api/user/${action}`, {
+      method: "POST",
+      body,
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      },
+      credentials: 'include'
+    })
+
+    const data = await res.json()
+    if (!res.ok)
+      throw new Error(data.message)
+
+    return data
+
+  } catch (error) {
+    console.error(error)
+
+    alert(error.message)
   }
 }
